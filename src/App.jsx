87bcd614983028e1db7b1151bc24c183ada05231
Jayanth Madupalli <jayanth.madupalli@gmail.com>
2023-09-24 09:54:26 -0500

    remove react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
-import Chat from "./components/Chat";
 
 function App() {
-  return (
-    <Routes>
-      <Route exact path="/" element={<Register />} />
-      <Route exact path="/chat" element={<Chat />} />
-    </Routes>
-  );
+  return <Register />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
